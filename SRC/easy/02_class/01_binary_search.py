37088f2501f29c36de0313e879e5c963adbea0b7
Animesh Bhadra <animeshb@archerimagine.com>
2020-06-14 16:15:50 +0530

    🐛 FIX: When the number to find is 73 , then it does not work

--- SRC/easy/02_class/01_binary_search.py
+++ SRC/easy/02_class/01_binary_search.py
@@ -5,16 +5,22 @@ def binary_search(sorted_list, number, debug=False):
     min = 0
     max = len(sorted_list) - 1
     result = -1
-    while max > min:
+    # Less than and equal 2 is important
+    while min <= max:
         mid = (min + max) // 2
         if debug:
             print(min, max, mid, sorted_list[min], sorted_list[max], sorted_list[mid])
         if sorted_list[mid] == number:
+            if debug:
+                print("FOUND::: ", mid)
             result = mid
+            break
         if sorted_list[mid] < number:
             min = mid + 1
         else:
             max = mid - 1 
+        if debug:
+            print("\t\t",min, max, mid, sorted_list[min], sorted_list[max], sorted_list[mid])
     return result
 
 if __name__ == "__main__":
@@ -45,7 +51,7 @@ if __name__ == "__main__":
     89,
     97,
 ]
-    number = 68
+    number = 73
 
-    index = binary_search(primes, number)
+    index = binary_search(primes, number, debug=True)
     print(index, primes[index])
