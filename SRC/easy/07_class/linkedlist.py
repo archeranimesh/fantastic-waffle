477a806573c88a572e9357f9d7d02ebb021b7ddb
Animesh Bhadra <animeshb@archerimagine.com>
2020-07-07 23:51:57 +0530

    📦 NEW: Detect Loop in a Linked list in 2 ways.

--- SRC/easy/07_class/linkedlist.py
+++ SRC/easy/07_class/linkedlist.py
@@ -32,6 +32,28 @@ class LinkedList(object):
 
         return rest
 
+    #  Floyd’s Cycle-Finding Algorithm
+    def detect_loop(self):
+        slow_p = self.head
+        fast_p = self.head
+
+        while slow_p and fast_p and fast_p.next:
+            fast_p = fast_p.next.next
+            slow_p = slow_p.next
+            if slow_p == fast_p:
+                return (True, slow_p.value)
+
+    # use set to detect loop
+    def detect_loop_set(self):
+        s = set()
+        temp = self.head
+        while temp:
+            if temp in s:
+                return (True, temp.value)
+            s.add(temp)
+            temp = temp.next
+        return (False, None)
+
     # https://stackoverflow.com/questions/28269643/python-unordered-listwith-nodes-str-method
     def __str__(self):
         temp = self.head
@@ -74,4 +96,19 @@ if __name__ == "__main__":
     print(str(linkedList))
     linkedList.head = linkedList.reverse_recursive(linkedList.head)
     print(str(linkedList))
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
+    n6 = Node(6)
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    n5.next = n6
+    n6.next = n4
+    linkedList = LinkedList(n1)
+    # print(linkedList.detect_loop())
+    print(linkedList.detect_loop_set())
 # 34:00
